6770975d206cf083cc7536d6977b1aa5d0cd1d7e
Hugues de Valon <hugues.devalon@gmail.com>
2015-11-25 22:19:41 +0100

    Ecriture des fonctions principales

--- src/puissance4.adb
+++ src/puissance4.adb
@@ -1,5 +1,7 @@
 with Participant, Puissance4;
+with Ada.Text_IO;
 use Participant;
+use Ada.Text_IO;
 
 package body Puissance4 is
 
@@ -31,5 +33,88 @@ package body Puissance4 is
 
         return E;
     end Jouer;
+
+    function Est_Gagnant(E : Etat; J : Joueur) return Boolean is
+        i : Natural := 0;
+        j : Natural := 0;
+        pions : Natural := 0;
+    begin
+        for i in E'range(1) loop
+            for j in E'range(2) loop
+                E(i,j) := Vide;
+            end loop;
+        end loop;
+    end Est_Gagnant;
+
+    function Est_Nul(E : Etat) return Boolean is
+
+    begin
+
+    end Est_Nul;
+
+    procedure Afficher(E : Etat) is
+        i : Natural := 0;
+        j : Natural := 0;
+    begin
+        for j in E'last(2)..E'first(2) loop
+            Put("|");
+            for i in E'last(1)..E'first(1) loop
+                if (E(i,j) = Joueur1) then
+                    Put("O");
+                elsif (E(i,j) = Joueur2) then
+                    Put("X");
+                else
+                    Put(" ");
+                end if;
+                Put_Line("|");
+            end loop;
+            New_Line;
+        end loop;
+    end Afficher;
+
+    procedure Affiche_Coup(C : in Coup) is
+    begin
+        if (C.j = Joueur1) then
+            Put_Line("Le joueur 1 a mis son pion sur la colonne " & Integer'Image(C.col));
+        elsif (C.j = Joueur2) then
+            Put_Line("Le joueur 2 a mis son pion sur la colonne " & Integer'Image(C.col));
+        else
+            Put_Line("Erreur.");
+        end if
+    end Affiche_Coup;
+
+    function Demande_Coup_Joueur1(E : Etat) return Coup is
+        col : Natural := 0;
+        trafalgar : Coup;
+    begin
+        Affiche(E);
+        Put_Line("Choix Joueur1 : ");
+        Get(col);
+        if (E(col, T'last(2)) /= Vide) then
+            Put_Line("Cette colonne est pleine.");
+            Demande_Coup_Joueur1(E);
+        end if;
+        trafalgar.j := Joueur1;
+        trafalgar.col := col;
+
+        return trafalgar;
+    end Demande_Coup_Joueur1;
+
+    function Demande_Coup_Joueur2(E : Etat) return Coup is
+        col : Natural := 0;
+        trafalgar : Coup;
+    begin
+        Affiche(E);
+        Put_Line("Choix Joueur2 : ");
+        Get(col);
+        if (E(col, T'last(2)) /= Vide) then
+            Put_Line("Cette colonne est pleine.");
+            Demande_Coup_Joueur1(E);
+        end if;
+        trafalgar.j := Joueur2;
+        trafalgar.col := col;
+
+        return trafalgar;
+    end Demande_Coup_Joueur1;       
 
 end Puissance4;
