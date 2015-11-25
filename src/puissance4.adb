5c914471d8929913464c7bb3ba59dbbf63e4863a
Hugues de Valon <hugues.devalon@gmail.com>
2015-11-25 20:44:30 +0100

    Fonction Jouer

--- src/puissance4.adb
+++ src/puissance4.adb
@@ -2,6 +2,7 @@ with Participant, Puissance4;
 use Participant;
 
 package body Puissance4 is
+
     procedure Initialiser(E : Etat) is
         i : Natural := 0;
         j : Natural := 0;
@@ -13,4 +14,22 @@ package body Puissance4 is
             end loop;
         end loop;
     end Initialiser;
+
+    function Jouer(E : Etat; C : Coup) return Etat is
+        jVide : Natural := 0;
+    begin
+        -- Si la colonne où l'on veut jouer est pleine
+        if (E'last(2) /= Vide) then
+            return null;
+        end if;
+        -- On se place sur la colonne du coup à jouer
+        -- et on cherche l'indice de la première case vide.
+        while E(C.col, jVide) /= Vide loop
+            jVide := jVide + 1;
+        end loop;
+        E(C.col, jVide) := C.j;
+
+        return E;
+    end Jouer;
+
 end Puissance4;
