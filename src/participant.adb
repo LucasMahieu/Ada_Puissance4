5ac538b197449e642fee1fb91242cd8371733d25
Hugues de Valon <hugues.devalon@gmail.com>
2015-11-25 20:01:35 +0100

    participant.adb

---
+++ src/participant.adb
@@ -0,0 +1,12 @@
+function Adversaire(J : Joueur) return Joueur is
+begin
+    if (J = Joueur1) then
+        return Joueur2;
+    end if;
+    if (j = Joueur2) then
+        return Joueur1;
+    end if;
+    if (J = Vide) then
+        return Vide;
+    end if;
+end Adversaire;
