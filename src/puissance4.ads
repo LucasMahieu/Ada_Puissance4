6e904a75eb3fc71fff852da7e327be5a5a5e32ac
Hugues de Valon <hugues.devalon@gmail.com>
2015-11-28 13:10:01 +0100

    adaptation main + p4 pour min-max

--- src/puissance4.ads
+++ src/puissance4.ads
@@ -1,4 +1,5 @@
 with Participant;
+with Liste_Generique;
 use Participant;
 
 generic
@@ -25,16 +26,22 @@ package Puissance4 is
     -- Calcule l'etat suivant en appliquant le coup
     function Jouer(E : Etat; C : Coup) return Etat;
     -- Indique si l'etat courant est gagnant pour le joueur J
-    function Est_Gagnant(E : Etat; J : Joueur) return Boolean; 
+    function Est_Gagnant(E : Etat; J : Joueur) return Boolean;
     -- Indique si l'etat courant est un status quo (match nul)
-    function Est_Nul(E : Etat) return Boolean; 
+    function Est_Nul(E : Etat) return Boolean;
     -- Fonction d'affichage de l'etat courant du jeu
     procedure Afficher(E : Etat);
     -- Affiche a l'ecran le coup passe en parametre
-    procedure Affiche_Coup(C : in Coup);   
+    procedure Affiche_Coup(C : in Coup);
     -- Retourne le prochaine coup joue par le joueur1
     function Demande_Coup_Joueur1(E : Etat) return Coup;
-    -- Retourne le prochaine coup joue par le joueur2   
-    function Demande_Coup_Joueur2(E : Etat) return Coup; 
+    -- Retourne le prochaine coup joue par le joueur2
+    function Demande_Coup_Joueur2(E : Etat) return Coup;
+    -- Implantation d'un package de liste de coups
+    with package Liste_Coups is new Liste_Generique(Coup, Affiche_Coup); 
+    -- Retourne la liste des coups possibles pour J a partir de l'etat
+    function Coups_Possibles(E : Etat; J : Joueur) return Liste_Coups.Liste;
+    -- Evaluation statique du jeu du point de vue de l'ordinateur
+    function Eval(E : Etat) return Integer;
 
 end Puissance4;
